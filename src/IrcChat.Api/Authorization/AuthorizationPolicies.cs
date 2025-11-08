dd2e4c602f5df872df4fd9002e46d317e5b20f4b
aguacongas <aguacongas@gmail.com>
2025-11-08 23:36:28 +0100

    refactor: add missing ns

--- src/IrcChat.Api/Authorization/AuthorizationPolicies.cs
+++ src/IrcChat.Api/Authorization/AuthorizationPolicies.cs
@@ -1,6 +1,9 @@
 /// <summary>
 /// Constantes pour les noms de policies d'autorisation
 /// </summary>
+
+namespace IrcChat.Api.Authorization;
+
 public static class AuthorizationPolicies
 {
     public const string CanModifyChannel = "CanModifyChannel";
