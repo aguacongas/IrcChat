4f5541775e9e7a3efc321dad33d635f18588469c
aguacongas <aguacongas@gmail.com>
2025-12-23 20:29:29 +0100

    style: reformat

--- src/IrcChat.Api/Migrations/20251223095753_AddDateOfBirth.cs
+++ src/IrcChat.Api/Migrations/20251223095753_AddDateOfBirth.cs
@@ -1,30 +1,29 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace IrcChat.Api.Migrations
+namespace IrcChat.Api.Migrations;
+
+/// <inheritdoc />
+public partial class AddDateOfBirth : Migration
 {
     /// <inheritdoc />
-    public partial class AddDateOfBirth : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<DateTime>(
-                name: "DateOfBirth",
-                table: "ReservedUsernames",
-                type: "timestamp with time zone",
-                nullable: false,
-                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
-        }
+        migrationBuilder.AddColumn<DateTime>(
+            name: "DateOfBirth",
+            table: "ReservedUsernames",
+            type: "timestamp with time zone",
+            nullable: false,
+            defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "DateOfBirth",
-                table: "ReservedUsernames");
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "DateOfBirth",
+            table: "ReservedUsernames");
     }
 }
